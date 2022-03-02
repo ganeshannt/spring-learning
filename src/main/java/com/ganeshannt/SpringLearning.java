39887581d3137089ba689f68764db96d5fdf986e
Nagarajan <ganeshan.nagarajan@capgemini.com>
2022-03-02 19:00:31 +0530

    Initial commit

---
+++ src/main/java/com/ganeshannt/SpringLearning.java
@@ -0,0 +1,12 @@
+package com.ganeshannt;
+
+/**
+ * @author Ganeshan Nagarajan
+ * @since 02-03-2022
+ */
+
+public class SpringLearning {
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+}
